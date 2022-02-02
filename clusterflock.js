eef1454307595bc6bf31286e621eaa462974ee4f
Christopher Steffen <cmsteffen@protonmail.com>
2022-02-01 18:17:28 -0600

    Update clusterflock.js
    The `del()` function was superfluous. The `get_task()` function was written inside-out. The "TEST" case was alphabetically out-of-order. Appalling! Refactored.

--- clusterflock.js
+++ clusterflock.js
@@ -3,7 +3,7 @@ const SPLASH = "\
  / __| |_  _ __| |_ ___ _ _| __| |___  __| |__\n\
 | (__| | || (_-<  _/ -_) '_| _|| / _ \\/ _| / /\n\
  \\___|_|\\_,_/__/\\__\\___|_| |_| |_\\___/\\__|_\\_\\\n\
-  by haxys                            v0.2.1\
+  by haxys                            v0.2.2\
 ";
 
 /** @param {import(".").NS } ns */
@@ -13,35 +13,29 @@ export async function main(ns) {
         await process_tasks();
     }
 
-    function del(filename, hostname) {
-        ns.run("/util/rm.js", 1, filename, hostname);
-    }
-
     async function get_task() {
         // Await a new assignment.
-        while(true) {
-            let new_task = ns.readPort(1);
-            if (new_task != "NULL PORT DATA"){
-                return JSON.parse(new_task);
-            } else {
-                await ns.asleep(1000);
-            }
+        let new_task = ns.readPort(1);
+        while (new_task == "NULL PORT DATA") {
+            await ns.asleep(1000);
+            new_task = ns.readPort(1);
         }
+        return JSON.parse(new_task);
     }
 
     async function process_tasks() {
         const task = await get_task();
         switch (task.type) {
-            case "TEST":
-                ns.tprintf("Test message: %s", task.message);
-                break;
             case "DELETE":
-                del(task.filename, task.hostname);
+                ns.run("/util/rm.js", 1, task.filename, task.hostname);
                 break;
             case "NUKABLE":
                 for (const target of task.hosts) {
                     ns.run("/ice/nuke.js", 1, target);
                 }
+                break;
+            case "TEST":
+                ns.tprintf("Test message: %s", task.message);
                 break;
             default:
                 ns.tprintf("Unknown task: %s", JSON.stringify(task));
